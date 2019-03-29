ed2e47e8e4764dd3dfa3bbc4e10e32cfb22db865
Starfall <Starfall121039156@gamil.com>
2019-03-29 14:06:28 +0800

    更新javadoc的Tag

--- EnumModule/src/main/java/com/starfall/enumeration/package-info.java
+++ EnumModule/src/main/java/com/starfall/enumeration/package-info.java
@@ -1,9 +1,9 @@
 /**
- * @Project： JavaProject
- * @Package： com.starfall.enumeration
- * @ClassName: package-info
- * @Author: StarFall
- * @DATE: 2019/3/24 23:44
- * @Description: 包说明：作为Enum枚举类型的包
+ * @project： JavaProject
+ * @package： com.starfall.enumeration
+ * @className: package-info
+ * @author: StarFall
+ * @date: 2019/3/24 23:44
+ * @description: 包说明：作为Enum枚举类型的包
  */
 package com.starfall.enumeration;
