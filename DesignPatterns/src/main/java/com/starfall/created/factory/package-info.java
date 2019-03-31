380704fd11e1511f9ab89deccb51acfe671a55eb
StarFall <Starfall121039156@gmail.com>
2019-03-31 23:36:19 +0800

    创建包及其说明类

---
+++ DesignPatterns/src/main/java/com/starfall/created/factory/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @project： JavaProject
+ * @package： com.starfall.created.factory
+ * @className： package-info
+ * @author： StarFall
+ * @date： 2019/3/31 23:34
+ * @description： 工厂类设计模式
+ */
+package com.starfall.created.factory;
