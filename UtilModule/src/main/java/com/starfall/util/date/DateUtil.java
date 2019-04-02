ad99f4840a2b73033fba5bcd93601fa3d108e48c
StarFall <Starfall121039156@gmail.com>
2019-04-02 23:53:52 +0800

    ADD：获取指定时间年月日

--- UtilModule/src/main/java/com/starfall/util/date/DateUtil.java
+++ UtilModule/src/main/java/com/starfall/util/date/DateUtil.java
@@ -1,5 +1,6 @@
 package com.starfall.util.date;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -13,14 +14,38 @@ import java.util.Date;
 public class DateUtil {
 
     /**
-     * 获取指定日期的年份
+     * 获取指定时间的年份
      *
      * @param date 指定日期
      * @return 年份
      */
-    public static int year(Date date) {
-
-        return 1;
+    public static int getYear(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.YEAR);
     }
 
+    /**
+     * 获取指定时间的月份
+     *
+     * @param date 指定日期
+     * @return 月份
+     */
+    public static int getMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MONTH) + 1;
+    }
+
+    /**
+     * 获取指定时间的日期
+     *
+     * @param date 指定时间
+     * @return 日期
+     */
+    public static int getDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
 }
--- UtilModule/src/test/java/com/starfall/util/date/test/DateUtilTest.java
+++ UtilModule/src/test/java/com/starfall/util/date/test/DateUtilTest.java
@@ -1,5 +1,12 @@
 package com.starfall.util.date.test;
 
+import com.starfall.util.date.DateUtil;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * @project： JavaProject
  * @package： com.starfall.util.date.test
@@ -9,4 +16,13 @@ package com.starfall.util.date.test;
  * @description： Date工具类测试类
  */
 public class DateUtilTest {
+
+    @Test
+    public void getYearTest() {
+        System.out.println(DateUtil.getYear(new Date()));
+        System.out.println(DateUtil.getMonth(new Date()));
+        System.out.println(DateUtil.getDay(new Date()));
+        System.out.println(DateUtil.getMonth(null));
+    }
+
 }
