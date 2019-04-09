a03b839109a5169ff89b37b77646149d1ab9e0af
StarFall <Starfall121039156@gmail.com>
2019-04-09 10:53:55 +0800

    ADD：常用常量：数字常量

---
+++ UtilModule/src/main/java/com/starfall/common/CommonConstant.java
@@ -0,0 +1,25 @@
+package com.starfall.common;
+
+/**
+ * @author StarFall
+ * @project JavaProject
+ * @package com.starfall.common
+ * @className CommonConstant
+ * @date 2019/4/9 10:31
+ * @description CommonConstant，通用常量
+ */
+public class CommonConstant {
+
+    public static final int NUM_ONE = 1;
+    public static final int NUM_TWO = 2;
+    public static final int NUM_THREE = 3;
+    public static final int NUM_FOUR=4;
+    public static final int NUM_FIVE = 5;
+    public static final int NUM_SIX=6;
+    public static final int NUM_SEVEN=7;
+    public static final int NUM_EIGHT = 8;
+    public static final int NUM_NINE=9;
+    public static final int NUM_TEN=10;
+    public static final int NUM_ELEVEN = 11;
+    public static final int NUM_TWELVE = 12;
+}
